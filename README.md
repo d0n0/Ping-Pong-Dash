85455aab0aec281da281ef0418309b773360eedd
xono <xononeet@gmail.com>
2017-03-12 18:59:06 +0900

    update

--- README.md
+++ README.md
@@ -1,14 +1,61 @@
 # Ping-Pong-Dash
 ### 駆け抜けろ ☆．。．:*･*ﾟ.:｡+ﾟ┌(　・。・)┘.☆
-<br>
+<br />
 
 ## description
 Amazon Dash Buttonと連携してベルを鳴らす簡単なプログラムです  
-<br>
+<br />
 
 ## require
 - Amazon Dash Button
-- node.js 6.x
+- nodejs 6.x
 - npm
 - mpg321
 - libpcap-dev
+
+## install 
+```
+# apt install -y mpg321 libpcap-dev
+$ git clone https://github.com/d0n0/Ping-Pong-Dash.git
+$ cd ./Ping-Pong-Dash
+$ npm install
+```
+
+## configure
+config.jsonにlistenするインターフェース名とボタンのMACアドレスを書き込む  
+<br />
+
+- インターフェースを確認
+```
+$ ifconfig
+or
+$ ip a
+```
+- Dash ButtonのMACアドレスを確認
+```
+要root権限
+$ sudo npm run scan
+コマンド入力後にボタンを押す
+```
+
+example)
+```
+$ cat ./config.json
+{
+  "listen_interface" : "wlan0",
+  "button_mac_addr"  : "34:d2:70:a4:8c:85"
+}
+```
+
+## run
+```
+要root権限
+$ sudo node pingpong.js
+```
+
+永続的にバックグラウンドで動かしたい場合  
+<br />
+```
+$ sudo npm install forever -g
+$ sudo forever pingpong.js
+```
--- pingpong.js
+++ pingpong.js
@@ -1,24 +1,22 @@
-// sudo node pingpong.js で実行
+const DashButton = require('dash-button');
+const moment = require('moment');
+const config = require('./config.json');
 
-
-const DashButton = require("dash-button");
+const LISTEN_INTERFACE = config.listen_interface;
+const BUTTON_MAC_ADDR = config.button_mac_addr;
 const COMMAND = "mpg321 ./pingpong.mp3";
-const DASH_BUTTON_MAC_ADDR = "34:d2:70:17:3a:ab";
-const LISTEN_INTERFACE = "wlx003a9daa7c68";
+let button = new DashButton(BUTTON_MAC_ADDR, {networkInterface: LISTEN_INTERFACE});
 
-let button = new DashButton(DASH_BUTTON_MAC_ADDR, {networkInterface: LISTEN_INTERFACE});
+console.log('interface       : %s', config.listen_interface);
+console.log('button_mac_addr : %s', config.button_mac_addr);
+console.log('\nlistening...\n');
 
-console.log("listening...");
-
-
-// 発火イベント
+// event
 button.addListener(() => {
-
-    const exec = require('child_process').exec;
-    exec(COMMAND, (err,stdout, stderr) => {
-        if(err) { console.log(err); }
-        console.log('ﾋﾟｰｰｰﾝ ﾎﾟｰｰｰﾝ.. ﾋﾟｰｰｰﾝ ﾎﾟｰｰｰﾝ..');
-        console.log(stdout);
-    });
-
+  const exec = require('child_process').exec;
+  exec(COMMAND, (err,stdout, stderr) => {
+    if(err) { console.log(err); }
+    let now = moment().format('YYYY/MM/DD HH:mm:ss');
+    console.log('Button is pressed (%s)', now);
+  });
 });
